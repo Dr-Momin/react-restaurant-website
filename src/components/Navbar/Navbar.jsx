fe48b5786a6a570d7c507a85601e0d76f4a4cf77
Momin Khan <mmomin.bscsf19@iba-suk.edu.pk>
2022-08-09 07:50:19 +0500

    Navbar Done...

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,100 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import './Navbar.css';
+import {GiHamburgerMenu} from 'react-icons/gi';
+import {MdOutlineRestaurantMenu} from "react-icons/md";
+import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+
+const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false);
+
+    return (
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
+                <img src={images.gericht} alt="applogo"/>
+            </div>
+
+            <ul className="app__navbar-links">
+                <li className={"p__opensans"}><a href="#home">Home</a></li>
+                <li className={"p__opensans"}><a href="#about">About</a></li>
+                <li className={"p__opensans"}><a href="#menu">Menu</a></li>
+                <li className={"p__opensans"}><a href="#awards">Awards</a></li>
+                <li className={"p__opensans"}><a href="#contact">Contact</a></li>
+            </ul>
+
+
+            <div className="app__navbar-login">
+                <a href="#login" className={"p__opensans"}>Login / Registration</a>
+                <div/>
+                <a href="/" className={"p__opensans"}>Book Table</a>
+            </div>
+
+
+            <div className="app__navbar-smallscreen">
+                <GiHamburgerMenu
+                    color={"white"}
+                    cursor={"pointer"}
+                    onClick={() => setToggle(prevState => !prevState)}
+                    display={toggle ? 'none' : ''}
+                />
+
+                {
+                    toggle &&
+                    <div className={"app__navbar-smallscreen_overlay flex__center slide-bottom"}>
+                        <MdOutlineRestaurantMenu
+                            onClick={() => setToggle(false)}
+                            className={"overlay__close"}
+                        />
+
+                        <ul className={"app__navbar-smallscreen_links"}>
+                            <li
+                                className={"p__opensans"}
+                                onClick={() => setToggle(false)}
+                            >
+                                <a href="#home">Home</a>
+                            </li>
+
+                            <li
+                                className={"p__opensans"}
+                                onClick={() => setToggle(false)}
+                            >
+                                <a href="#about">About</a>
+                            </li>
+
+                            <li
+                                className={"p__opensans"}
+                                onClick={() => setToggle(false)}
+                            >
+                                <a href="#menu">Menu</a>
+                            </li>
+
+                            <li
+                                className={"p__opensans"}
+                                onClick={() => setToggle(false)}
+                            >
+                                <a href="#awards">Awards</a>
+                            </li>
+
+                            <li
+                                className={"p__opensans"}
+                                onClick={() => setToggle(false)}
+                            >
+                                <a href="#contact">Contact</a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                }
+
+
+            </div>
+
+        </nav>
+    );
+};
+
 
 export default Navbar;
